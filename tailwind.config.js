2283f79c90c1c7c69f1bc894b2a47b23f7f27b51
tms <tmsnet@126.com>
2021-10-13 16:14:39 +0800

    learnin tailwind in udemy

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      green: '#19cb98',
+      purple: '#754ffe',
+      fontal: '#f5f4f8',
+      white: '#ffffff',
+      lightPurple: '#e8deff',
+      footerColor: '#edebf1',
+      gray: '#c4c4c4'
+    },
+    fontFamily: {
+      'ham': [
+        'Harmattan', 'sans-serif'
+      ]
+    },
+    container: {
+      center: true,
+      padding: '4.5rem'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
